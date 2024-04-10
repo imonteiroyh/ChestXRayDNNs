f49d7c2e946cc7c0c26f9e184e056b523a874795
imonteiroyh <imonteiroyh@gmail.com>
2024-04-10 12:09:36 -0300

    style: add docstring and typing

--- xrkit/utilities/array.py
+++ xrkit/utilities/array.py
@@ -1,7 +1,19 @@
 import numpy as np
 
 
-def handle_empty_array(array):
+def handle_empty_array(array: np.ndarray) -> np.ndarray:
+    """
+    Handle empty arrays by filling them with a default value if they contain only zeros.
+
+    Args:
+        array (np.ndarray):
+            Input array.
+
+    Returns:
+        np.ndarray:
+            Array with zeros replaced by a default value if the array was empty.
+    """
+
     if np.count_nonzero(array) == 0:
         array[0, 0] = 1
 
--- xrkit/utilities/tensor.py
+++ xrkit/utilities/tensor.py
@@ -1,7 +1,24 @@
+from typing import Tuple
+
+import torch
 import torch.nn.functional as F
 
 
-def resize_4d_tensor(tensor, size):
+def resize_4d_tensor(tensor: torch.Tensor, size: Tuple[int, int, int]) -> torch.Tensor:
+    """
+    Resize a 4D tensor to a specified size using trilinear interpolation.
+
+    Args:
+        tensor (torch.Tensor):
+            Input tensor with shape (batch_size, channels, depth, height, width).
+        size (Tuple[int, int, int]):
+            Desired size of the output tensor in the format (depth, height, width).
+
+    Returns:
+        torch.Tensor:
+            Resized tensor with the specified size.
+    """
+
     tensor = tensor.unsqueeze(0)
 
     resized_tensor = F.interpolate(tensor, size=size, mode="trilinear", align_corners=False)
