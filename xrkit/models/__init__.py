5aa09fe876866c15a7ba9b0e9a803c2dac2eae7b
imonteiroyh <imonteiroyh@gmail.com>
2024-07-22 22:37:47 -0300

    update training notebooks

--- xrkit/models/__init__.py
+++ xrkit/models/__init__.py
@@ -3,14 +3,14 @@ from xrkit.models.lightning.inceptionresnetv2unet import InceptionResNetV2UNetMo
 from xrkit.models.lightning.inceptionv3unet import InceptionV3UNetModel
 from xrkit.models.lightning.mobilenetv2unet import MobileNetV2UNetModel
 from xrkit.models.lightning.nasnetlargeunet import NASNetLargeUNetModel
-from xrkit.models.lightning.resnet152v2unet import ResNet152V2UNetModel
+from xrkit.models.lightning.resnet152unet import ResNet152UNetModel
 from xrkit.models.lightning.vgg19unet import VGG19UNetModel
 from xrkit.models.lightning.xceptionunet import XceptionUNetModel
 
 __all__ = [
     "DenseNet201UNetModel",
     "NASNetLargeUNetModel",
-    "ResNet152V2UNetModel",
+    "ResNet152UNetModel",
     "VGG19UNetModel",
     "XceptionUNetModel",
     "InceptionV3UNetModel",
